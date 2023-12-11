50bfa44df932879a980a43513eb0841ee71a6902
MohdAftab011 <125395803+MohdAftab011@users.noreply.github.com>
2023-12-11 19:58:55 +0000

    short hand if-else

--- Control2/01-If.js
+++ Control2/01-If.js
@@ -15,4 +15,38 @@ if(score>99){
 }else{
     console.log("Score not enough")
 }
-console.log(`Your score is ${score} and your life left are ${life}`)
+console.log(`Your score is ${score} and your life left are ${life}`);
+
+//short hands
+
+let balance = 500
+if(balance>100) console.log("Balance is above 500");
+            //OR
+if(balance>100) 
+    console.log("Balance is above 500"),
+    console.log("Balance is above 500"),
+    console.log("Balance is above 500")
+
+// Rating system for PizzaHut    
+
+let getStar_fromUser = "2"
+let star = parseInt(getStar_fromUser)
+if(star==1) console.log("poor")
+else if(star==2) {
+    console.log("poor");
+}
+else if(star==3) {
+    console.log("avg");
+}
+else if(star==4) {
+    console.log("great");
+}
+else if(star==5) {
+    console.log("awesome");
+}
+else if(star==6) {
+    console.log("delicious");
+}
+else{
+    console.log("I don't know that rating");
+}
